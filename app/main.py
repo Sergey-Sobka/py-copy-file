67a458807df7046f89c595e98280eb1f8eb5483c
Sergey <sobka2702@gmail.com>
2026-04-15 16:37:36 +0300

    full logic for copy file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-# write your code here
+import os
+
+
+def copy_file(command: str) -> None:
+    parts = command.split()
+    if len(parts) > 2:
+        print(parts)
+        src = parts[0]
+        dst = parts[1]
+        print(src, dst)
+    if len(parts) != 3 or parts[0] != "cp":
+        return
+
+    _, src, dst = parts
+
+    if src == dst or not os.path.exists(src):
+        return
+
+    with open(src, "r") as file_in, open(dst, "w") as file_out:
+        file_out.write(file_in.read())
